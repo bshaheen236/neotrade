1dcd289876c7697b7053569f77f8d5097c495ca9
shaheen bano <bshaheen236@gmail.com>
2023-05-03 23:15:38 +0530

    first commit

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-const env = require('dotenv').config();
-const db = require('./utils/dbconnection');
-const port = process.env.PORT;
-
-const app = require('./app');
-
-app.listen(port, () => console.log(`App is running on port ${port}`));
